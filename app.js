7870eda4e9c92de89bb759b73cb6312a8ead2c0f
destinydung0903@gmail.com <destinydung0903@gmail.com>
2023-01-06 10:51:05 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,47 @@
+//include exprss from node_modules
+const express = require("express");
+const app = express();
+
+//variables
+const restaurantList = require("./restaurant.json");
+
+//Define server related variables
+const port = 3000;
+
+//require express-handlebars
+const exphbs = require("express-handlebars");
+
+//setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//setting static files
+app.use(express.static("public"));
+
+//request & response
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+//針對特定餐廳設定不同路由 & res
+app.get("/restaurants/:id", (req, res) => {
+  let restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.id
+  );
+  res.render("show", { restaurant });
+});
+
+//產生搜尋的路由
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  let findRestaruant = restaurantList.results.filter((restaurant) =>
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase().trim())
+  );
+
+  res.render("index", { restaurants: findRestaruant, keyword });
+});
+
+//start and listen the server
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`);
+});
